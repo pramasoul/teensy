52a7bc52ed21f773b0f7c3fdc361c9e38e48593f
Tom Soulanille <soul@prama.com>
2014-06-01 10:37:23 -0700

    LED show the slug sensors that see the slug

--- bouncy/bouncy.c
+++ bouncy/bouncy.c
@@ -51,9 +51,53 @@ void blink_n_times(int v) {
     _delay_ms(100);
     LED_TOGGLE;
   }
-  _delay_ms(100);
+  _delay_ms(200);
 }
   
+
+void drive_slug_left(void) {
+  PORTC |=  0x80;
+  PORTC &= ~0x40;
+}
+
+void drive_slug_right(void) {
+  PORTC |=  0x40;
+  PORTC &= ~0x80;
+}
+
+void coast_slug(void) {
+  PORTC |= 0xc0;
+}
+
+void brake_slug(void) {
+  PORTC &= ~0xc0;
+}
+
+void init_slug_sensors(void) {
+  DDRB &= ~((1<<7) | 0xf);
+  PORTB &= ~0x0f;
+}
+
+int read_slug_sensor(int n) {
+  int v;
+  n &= 3;
+  PORTB &= ~(1<<n);
+  DDRB |= 1<<n;
+  _delay_us(1000);
+  v = PINB & (1<<7);
+  DDRB &= ~(1<<n);
+#if 0
+          char buf[6];
+	  buf[0] = '0' + n;
+	  buf[1] = ':';
+	  buf[2] = HEX((v >> 4) & 0xf);
+	  buf[3] = HEX(v & 0xf);
+	  buf[4] = '\r';
+	  buf[5] = '\n';
+	  usb_serial_write((unsigned char *)buf, 6);
+#endif
+  return v ? 0 : 1;
+}
 
 int main(void)
 {
@@ -76,6 +120,16 @@ int main(void)
 	//	while (!usb_configured()) /* wait */ ;
 	_delay_ms(1000);
 
+	init_slug_sensors();
+
+	while (1) {
+	  for (int i=0; i<4; i++) {
+	    if (read_slug_sensor(i)) {
+	      blink_n_times(i+1);
+	    }
+	  }
+	}
+
 	// Set up the H-bridge - a Vishay Si9986
 	// It's connected to PC6 and PC7
 	PORTC = 0;
@@ -83,15 +137,15 @@ int main(void)
 
 	while (1) {
 	  LED_ON;
-	  PORTC |= 0x80;
+	  PORTC = 0x80;
 	  _delay_ms(400);
-	  PORTC &= 0xC0;
+	  PORTC = 0xC0;
 	  LED_OFF;
 	  _delay_ms(1000);
 	  LED_ON;
-	  PORTC |= 0x40;
+	  PORTC = 0x40;
 	  _delay_ms(400);
-	  PORTC &= 0xC0;
+	  PORTC = 0xC0;
 	  LED_OFF;
 	  _delay_ms(1000);
 	}
